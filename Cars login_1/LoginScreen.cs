115ec0476a46eed13229d574b45ab87785f57919
ernestovalles14 <ernestovalles14@gmail.com>
2019-06-28 14:42:13 -0400

    Added CatalogPlaceHolder screen and Updated database schema

---
+++ Cars login_1/LoginScreen.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cars_login_1
+{
+    public partial class Form1 : Form
+    {
+        SqlCommand sqlCommand = new SqlCommand();
+        SqlConnection sqlConnection = new SqlConnection();
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_login_Click(object sender, EventArgs e)
+        {
+            sqlConnection.ConnectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=" +
+                "C:\\Users\\Ernest O\\source\\repos\\Ernest157\\classic - cars\\Classic_Cars\\CarsDB.mdf;" +
+                "Integrated Security=True;Connect Timeout=30";
+
+            
+        }
+    }
+}
